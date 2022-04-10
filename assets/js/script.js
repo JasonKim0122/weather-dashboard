1bd46025fb6bcd830dee8a96c63a4802219b3486
JasonKim0122 <JasonKim0122@gmail.com>
2022-04-10 17:00:23 -0500

    fixed syntax. and also created an eventHandler for button submission

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,32 +7,34 @@ var searchCityEl = document.getElementById("search-input");
 var searchedCitiesEl = document.getElementById("searchedCity");
 var cityInputEl = document.getElementById("city");
 var dateInputEl = document.getElementById("currentDate");
+var searchFormEl = document.querySelector(".search");
+var searchBtnEl = document.getElementById("search-btn");
 
 //Function for getting info from API
-function getWeather (cityName) {
+function getWeather (cityLookUp) {
     //Need to first Format the API Url
-    var apiUrl = weatherAPI + cityName + "&" + myKey + "&units=imperial"; 
+    var apiUrl = weatherAPI + cityLookUp + "&" + myKey + "&units=imperial"; 
 
     //use fetch to make a request to the url
     fetch(apiUrl).then(function(response){
         if (response.ok) {
             return response.json().then(function (response) {
-                cityInputEl.innerHTML(response.name);
+                cityInputEl.innerHTML =response.name;
                 
                 //displaying date along with city
                 var currentTime = response.dt;
                 var currentDate = moment.unix(currentTime).format("MM/DD/YY");
-                dateInputEl.innherHTML(currentDate);
+                dateInputEl.innherHTML = currentDate;
 
                 //displaying info
-                var currentTemp =getElementById("temp");
-                currentTemp.innerHTML(response.main.temp + " \u00B0F");
+                var currentTemp =document.getElementById("temp");
+                currentTemp.innerHTML= (response.main.temp + " \u00B0F");
 
-                var currentWind=getElementById("wind");
-                currentWind.innerHTML(response.wind.speed + " MPH");
+                var currentWind= document.getElementById("wind");
+                currentWind.innerHTML =(response.wind.speed + " MPH");
 
-                var currentHumidity=getElementById("humidity");
-                currentHumidity.innherHTML(response.main.humidity + " %");
+                var currentHumidity= document.getElementById("humidity");
+                currentHumidity.innherHTML = response.current.humidity;
 
                 //need coordinates to call UV index correctly
                 var lat = response.coord.lat;
@@ -59,8 +61,8 @@ function getForecast (lat, lon) {
             var currentTime = response.daily[i].dt;
             var currentDate = moment.unix(currentTime).format("MM/DD/YY");
 
-            var dayEl = getElementById("day" + i);
-            dayEl.innherHTML(currentDate); 
+            var dayEl = document.getElementById("day" + i);
+            dayEl.innherHTML= currentDate; 
 
             //displaying weather pic
             var iconForWeatherUrl = "http://openweathermap.org/img/wn/" + response.daily[i].weather[0].icon + "@2x.png";
@@ -68,13 +70,13 @@ function getForecast (lat, lon) {
 
             //displaying current day temperature
             var tempToday = response.daily[i].temp.day + "\u00B0F"; 
-            var tempDayEl = getElementById("tempDay" + i);
-            tempDayEl.innerHTML(tempToday);
+            var tempDayEl = document.getElementById("tempDay" + i);
+            tempDayEl.innerHTML =tempToday;
 
             //showing humidity
             var humidityToday = response.daily[i].humidity;
-            var humidityDayEl = getElementById("humidityDay" + i);
-            humidityDayEl.innerHTML(humidityToday);
+            var humidityDayEl = document.getElementById("humidityDay" + i);
+            humidityDayEl.innerHTML = humidityToday;
         }
     });
 };
@@ -85,8 +87,8 @@ function getUVIndex (lat, lon) {
     fetch(apiUrl).then(function(response){
         return response.json();
     }).then(function(response){
-        var currentIndex = getElementById("uv");
-        currentIndex.innerHTML(response.value);
+        var currentIndex = document.getElementById("uv");
+        currentIndex.innerHTML= response.value;
     });
 };
 
@@ -110,5 +112,12 @@ function getUVIndex (lat, lon) {
 
 
 
-//Call functions
-getWeather();
+//Event Handlers
+function searchFormHandler (event) {
+    var cityLookUp = document.getElementById("search-input").value;
+    getWeather(cityLookUp); 
+};
+
+searchBtnEl.addEventListener("click",function(event) {
+    searchFormHandler(event);
+});
