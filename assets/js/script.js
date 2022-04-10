b8efff3b5a74efa3583ad3eabef2ba6f7e4f6eaf
JasonKim0122 <JasonKim0122@gmail.com>
2022-04-10 13:23:18 -0500

    added borders to card and started pseudocode for js

---
+++ assets/js/script.js
@@ -0,0 +1,35 @@
+// Variables
+var myKey = "fe7a42d4613005e1775fd8f48c765d89";
+var weatherAPI = "https://api.openweathermap.org/data/2.5/weather?q=";
+var weatherUV = "https://api.openweathermap.org/data/2.5/uvi?";
+var weatherForecast ="https://api.openweathermap.org/data/2.5/onecall?";
+
+
+//Function for getting info from API
+
+
+
+
+
+
+
+//Creating buttons for recent search so user can easily click back 
+
+
+
+
+
+//Save to localStorage
+
+
+
+
+
+//Load localStorage
+
+
+
+
+
+
+//Event Handlers
